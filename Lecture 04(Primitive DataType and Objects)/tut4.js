ebdf6280238e197a97cf6c477bd5ebeced591d2f
Asad Khan <asadlinkinpark9@gmail.com>
2024-02-05 01:04:48 +0500

    committed

---
+++ Lecture 04(Primitive DataType and Objects)/tut4.js
@@ -0,0 +1,24 @@
+// NN,BB,SS,U
+
+let a = null;
+let b = 123;
+let c = true; //OR FALSE
+let d = BigInt("1000") + BigInt("2000")
+let e = "Asad"
+let f = Symbol("I drive a Car")
+let g = undefined
+console.log(a , b ,c ,d ,e ,f ,g) 
+console.log(typeof b)
+
+//OBJECTS
+
+const items = {
+    "Asad" : true,
+    "Khan" : false,
+    "Ali" : 67,
+    "Ahmed": undefined
+}
+console.log(items["Asad"])
+console.log(items["Khan"])
+console.log(items["Ali"])
+console.log(items["Ahmed"])
