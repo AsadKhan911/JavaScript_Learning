09e6ca69fc6de3b3839c495506afdc7ab516379e
Asad Khan <asadlinkinpark9@gmail.com>
2024-02-04 18:04:00 +0500

    committed

---
+++ Lecture 03(Const, let&var)/tut3.js
@@ -0,0 +1,16 @@
+console.log("Const , Var & Let")
+
+// let a = 10
+// {
+//     let a = 20
+//     console.log(a)
+// }
+// console.log(a)
+
+// let a = 10
+// a = 8
+// console.log(a)
+
+// const b =20
+// b = 10
+// console.log(b)
