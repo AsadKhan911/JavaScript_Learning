483852d9b8c5a70fe1eb0caacb57e7e722128653
Asad Khan <asadlinkinpark9@gmail.com>
2024-02-07 03:39:55 +0500

    committed

---
+++ Lecture 06(Operators&Expressions)/tut6.js
@@ -0,0 +1,53 @@
+12
+"Asad"
+false
+//These are expressions in js , like we can write any datatype without giving them variable , js will not throw any error
+
+//OPERATORS
+let a = 20
+let b =4
+//ARITHMETRIC OPERATORS
+console.log("a+b = " , a+b)
+console.log("a-b = " , a-b)
+console.log("a*b = " , a*b)
+console.log("a/b = " , a/b)
+console.log("a**b = " , a**b) //a to the power b
+console.log("a%b = " , a%b)
+console.log("a++ = " , a++) //post increment
+console.log(a)
+console.log("++a = " , ++a) //pre increment
+console.log("b-- = " , b--) //post decrement
+console.log(b)
+console.log("--b = " , --b) //pre decrement
+
+//ASSIGNMENT OPERATORS
+let c = 6
+c+= 1
+console.log(c)
+c-=2
+console.log(c)
+c*=2
+console.log(c)
+c/=2
+console.log(c)
+c**=2
+console.log(c)
+c%=2
+console.log(c)
+
+//COMPARISON OPERATORS
+// let comp1 = 5
+// let comp2 ="5"
+// console.log(comp1!=comp2)
+// console.log(comp1==comp2)
+// console.log(comp1===comp2) //it also compare datatypes then show results.
+
+//LOGICAL OPERATORS
+let x = 5
+let y = 6
+console.log(x<y && x==5)
+console.log(x>y || x==5)
+console.log(x!==5) //it is true but it will make it false
+
+
+ 
