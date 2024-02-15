b2f44564acb609344dd19a5f2d5bdaa59d542a42
Asad Khan <asadlinkinpark9@gmail.com>
2024-02-16 02:28:57 +0500

    committed

---
+++ Lecture 13(Intro To Strings)/tut13.js
@@ -0,0 +1,24 @@
+let name = "Asad"
+console.log(name.length)
+console.log(name[0])
+console.log(name[1])
+console.log(name[2])
+console.log(name[3])
+
+let name2 = 'Asad'
+console.log(name2)
+
+//TEMPELATE LITERALS
+let boy0 = "Adil"
+let boy1 = "Khan"
+let sentence = `boy0 is a friend of boy1`
+console.log(sentence)
+
+sentence = `${boy0} is a friend of ${boy1}` //STRING INTERPOLATION,variable within a string.
+console.log(sentence)
+
+//ESCAPE SEQUENCE CHARACTER.
+let ESC = 'This is Asad\'s Book'
+console.log(ESC)
+let ESC1 = 'This is Ahmed\'s Book and\nThis is Khan\'s Book'
+console.log(ESC1)
