5a282c8ceb30d5e159ed01a0e9fab2975c0925c9
Asad Khan <asadlinkinpark9@gmail.com>
2024-03-01 10:33:53 +0500

    committed

---
+++ Lecture 27(BOM,DOM&Windows Object)/tut27.js
@@ -0,0 +1,6 @@
+console.log(window)
+console.log(window.document)
+//0R
+console.log(document.body)
+document.body.style.background = "pink"
+location.href = "https://www.facebook.com/" //BOM PART, It will redirect to facebook website.
