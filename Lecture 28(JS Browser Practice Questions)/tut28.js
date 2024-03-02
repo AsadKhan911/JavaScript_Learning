8d30999dce67c932b91155d76ced764e36b80734
Asad Khan <asadlinkinpark9@gmail.com>
2024-03-03 00:56:07 +0500

    committed

---
+++ Lecture 28(JS Browser Practice Questions)/tut28.js
@@ -0,0 +1,45 @@
+// const prompt =require("prompt-sync")();
+
+//Q1 AND Q2
+// let runAgain = true
+
+// while(runAgain)
+// {
+// let age = prompt("Enter Your Age!")
+// age = Number.parseInt(age)
+
+// if(age>=18)
+// {
+//     alert("You Can Drive!")
+// }
+// else if (age<0)
+// {
+//     console.error("NEGATIVE AGE!") //Q3
+// }
+// else
+// {
+//     alert("You Can't Drive!")
+// }
+// runAgain = confirm("Do you want to insert age again?")
+// }
+
+// //Q4
+// let num 
+// num = prompt("ENTER A NUMBER")
+
+// if(num>4)
+// {
+//     location.href="https://www.google.co.uk/"
+// }
+// else
+// {
+//     alert("Numer is less than 4")
+// }
+
+//Q5
+let color
+color = prompt("ENTER THE COLOR")
+
+document.body.style.background = color
+
+
