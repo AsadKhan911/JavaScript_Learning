b6b440711c7e8b685994ffa4b641b1d93128e24d
Asad Khan <asadlinkinpark9@gmail.com>
2024-03-06 17:47:53 +0500

    committed

---
+++ Lecture 31(Accessing Children of an Element)/tut31.js
@@ -0,0 +1,7 @@
+console.log(document.body.firstChild)
+console.log(document.body.lastChild)
+console.log(document.body.childNodes)
+
+//CONVERTING child nodes into array
+let arr = Array.from(document.body.childNodes)
+console.log(arr)
