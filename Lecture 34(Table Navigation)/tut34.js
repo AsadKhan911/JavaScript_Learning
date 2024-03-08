f53a2bf6760ffdf9d27a58718060037db3ac6aa2
Asad Khan <asadlinkinpark9@gmail.com>
2024-03-09 04:12:40 +0500

    committed

---
+++ Lecture 34(Table Navigation)/tut34.js
@@ -0,0 +1,7 @@
+let t = document.body.firstElementChild.firstElementChild //refering to table
+console.log(t)
+console.log(t.rows)
+console.log(t.tHead)
+console.log(t.tFoot)
+console.log(t.tHead.firstElementChild) //for accessing <tr></tr>
+console.log(t.tBodies)
