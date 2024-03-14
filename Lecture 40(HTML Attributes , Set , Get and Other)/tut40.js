43de4644b9e363ecb9f4c57eb69b3f452c5a2116
Asad Khan <asadlinkinpark9@gmail.com>
2024-03-15 01:10:03 +0500

    committed

---
+++ Lecture 40(HTML Attributes , Set , Get and Other)/tut40.js
@@ -0,0 +1,23 @@
+let first = document.getElementById("first")
+
+// let a = first.getAttribute("class")
+// console.log(a)
+
+// let b = first.hasAttribute("class")
+// console.log(b)
+
+// b = first.hasAttribute("id")
+// console.log(b)
+
+
+// first.setAttribute("class" , "two")
+
+// // first.removeAttribute("class")
+
+// console.log(first.attributes)
+
+console.log(first.dataset)
+console.log(first.dataset.myName)
+console.log(first.dataset.lastName)
+
+
