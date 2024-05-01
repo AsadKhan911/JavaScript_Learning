0e719899a92d50cfec2035c875cfaccb26375925
Asad Khan <asadlinkinpark9@gmail.com>
2024-05-01 19:56:42 +0500

    committed

---
+++ LECTURE 60(The Finally Clause)/tut60.js
@@ -0,0 +1,21 @@
+// try {
+//     let p = 10
+//     console.log(p)
+// } catch (error) {
+//     console.log("This is an error")
+// }
+// finally{
+//     console.log("I am a good boy")
+// }
+
+try {
+    console.log(a)
+} catch (error) {
+    console.log("This is an error")
+}
+finally{
+    console.log("I am a good boy")
+    //Close the file
+    //Exit the loop
+    //Write to the log file
+}
